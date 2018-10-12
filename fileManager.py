c12f4609c70a34d45bf2240da5630c881d6e154b
Milosz <milosz.fraszczak@gmail.com>
2018-10-12 05:03:36 -0400

    refactor

---
+++ fileManager.py
@@ -0,0 +1,90 @@
+import os
+
+
+class FilesManger:
+    def __init__(self, folder_name, file_names):
+        self.dictionary_path = "dictionary.txt"
+        self.folder_with_dictionaries = folder_name
+        if not os.path.exists("./" + folder_name):
+            self.create_dictionaries(file_names)
+
+    def create_dictionaries(self, names):
+        self.create_directories(names)
+        with open(self.dictionary_path) as f:
+            main_dictionary = list(f)
+            words = self.slice_list(main_dictionary, names)
+
+            for i in words:
+                if i:
+                    self.save_words(i)
+
+    def create_directories(self, names):
+        for name in names:
+            try:
+                os.makedirs("./" + self.folder_with_dictionaries + "/" + name)
+            except FileExistsError as ex:
+                print(ex)
+
+    def slice_list(self, whole_list, characters):
+        tab = []
+        tup = tuple([x for x in characters])
+        whole_list = [x for x in whole_list if x.startswith(tup)]
+
+        for char in characters:
+            inner_tab = []
+            for i in range(len(whole_list)):
+                if char == whole_list[i][0]:
+                    inner_tab.append(whole_list[i])
+                else:
+                    whole_list = whole_list[i:]
+                    break
+            tab.append(inner_tab)
+
+        return tab
+
+    def save_words(self, table):
+        tab = self.divide_by_length(table)
+        sign = table[0][0]
+
+        for i in range(len(tab) - 1):
+            if tab[i]:
+                self.save(tab[i], sign, str(i + 2))
+
+        self.save(tab[7], sign, 'more')
+
+    def divide_by_length(self, words):
+        tab = []
+        for i in range(8):
+            tab.append([])
+
+        for word in words:
+            if 10 < len(word):
+                tab[-1].append(word)
+            elif 2 < len(word):
+                tab[len(word) - 3].append(word)
+        return tab
+
+    def save(self, tab, sign, length):
+        with open("./" + self.folder_with_dictionaries + "/" + sign + "/" + str(length) + ".txt", 'w') as f:
+            for i in range(len(tab)):
+                f.write(tab[i])
+
+    # def read_word_list(self, name, length):
+    #     tab = None
+    #     try:
+    #         with open("./" + self.folder_with_dictionaries + "/" + name + "/" + str(length) + ".txt") as f:
+    #             tab = f.read().splitlines()
+    #     except FileNotFoundError as ex:
+    #         print(ex)
+    #
+    #     return tab
+
+    def read_dictionary(self, sign, length):
+        tab = None
+        try:
+            with open("./" + self.folder_with_dictionaries + "/" + sign + "/" + str(length) + ".txt") as f:
+                tab = f.read().splitlines()
+        except FileNotFoundError as ex:
+            print(ex)
+
+        return tab
--- solver.py
+++ solver.py
@@ -1,134 +1,39 @@
 import itertools
 import operator
-import os
 
-
-class FilesManger:
-    def __init__(self, folder_name, file_names):
-        self.directory_path = "dictionary.txt"
-        self.dictionaries_folder_name = folder_name
-        if not os.path.exists("./" + folder_name):
-            self.create_dictionaries(file_names)
-
-    def create_dictionaries(self, names):
-        self.create_directories(names)
-        with open(self.directory_path) as f:
-            main_dictionary = list(f)
-            words = self.create_list_of_words(main_dictionary, names)
-
-            for i in words:
-                if i:
-                    self.save_sign(i)
-
-    def create_directories(self, names):
-        for name in names:
-            try:
-                os.makedirs("./" + self.dictionaries_folder_name + "/" + name)
-            except FileExistsError as ex:
-                print(ex)
-
-    def create_list_of_words(self, full_tab, names):
-        tab = []
-        for char in names:
-            innerTab = []
-            for i in range(len(full_tab)):
-                if char == full_tab[i][0]:
-                    innerTab.append(full_tab[i])
-                else:
-                    tab.append(innerTab)
-                    full_tab=full_tab[i:]
-                    break
-        return tab
-
-    def read_word_list(self, name, length):
-        tab = None
-        try:
-            with open("./" + self.dictionaries_folder_name + "/" + name + "/" + str(length) + ".txt") as f:
-                tab = f.read().splitlines()
-        except FileNotFoundError as ex:
-            print(ex)
-
-        return tab
-
-    def save_sign(self, SignedTab):
-        tab = []
-        sign = SignedTab[0][0]
-
-        for i in range(8):
-            tab.append([])
-
-        i = 0
-        while i < len(SignedTab):
-            if 10 > len(SignedTab[i]):
-                if 2 < len(SignedTab[i]):
-                    tab[len(SignedTab[i]) - 3].append(SignedTab[i])
-            else:
-                tab[7].append(SignedTab[i])
-            i += 1
-
-        for i in range(7):
-            if tab[i]:
-                self.save(tab[i], sign, str(i + 2))
-
-        self.save(tab[7], sign, 'more')
-
-    def save(self, tab, sign, length):
-        with open("./div/" + sign + "/" + length + ".txt", 'w') as f:
-            for i in range(len(tab)):
-                f.write(tab[i])
+from fileManager import FilesManger
 
 
 class Solver:
-    def __init__(self):
-        self.alphabeth = "aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż"
-        FilesManger("div", self.alphabeth)
-        pass
+    def __init__(self, alphabeth="abcdefghijklmnopqrstuvwxyz", dictionaries_folder_name="div"):
+        self.alphabeth = alphabeth
+        self.folder_name = dictionaries_folder_name
+        self.fm = FilesManger(self.folder_name, self.alphabeth)
 
-    def prepare_list_of_permutations(self, signs, len):
-        sequence = [''.join(p) for p in itertools.permutations(signs, len)]
+    @staticmethod
+    def prepare_list_of_permutations(signs, length):
+        sequence = [''.join(p) for p in itertools.permutations(signs, length)]
         return list(map(operator.itemgetter(0), itertools.groupby(sorted(sequence))))
 
-    def create_dictionaries(self):
-        pass
+    def get_correct_words(self, letters, length):
+        permutation_tab = self.prepare_list_of_permutations(letters, length)
 
-    def read(self, sign, length):
-        tab = None
-        try:
-            with open("./div/" + sign + "/" + str(length) + ".txt") as f:
-                tab = f.read().splitlines()
-        except FileNotFoundError:
-            pass
-
-        return tab
-
-    def equalizer(self, word, length):
+        global literals_tab
         word_list = []
-        permutation_tab = self.prepare_list_of_permutations(word, length)
-        j = 0
-        sign = permutation_tab[0][0]
-        literals_tab = self.read(sign, length)
-
-        w = 0
-        while w < len(permutation_tab):
-            if sign == permutation_tab[w][0]:
+        for char in self.alphabeth:
+            if char in letters:
+                literals_tab = self.fm.read_dictionary(char, length)
                 if literals_tab:
-                    if permutation_tab[w] in literals_tab:
-                        word_list.append(permutation_tab[w])
-                w += 1
-            else:
-
-                while True:
-                    j += 1
-                    if j >= len(self.alphabeth) - 1:
-                        return word_list
-                    if self.alphabeth[j] in word:
-                        sign = self.alphabeth[j]
-                        literals_tab = self.read(sign, length)
-                        break
+                    for word in permutation_tab:
+                        if word in literals_tab:
+                            word_list.append(word)
 
         return word_list
 
 
 if __name__ == "__main__":
     solver = Solver()
-    print("\n".join(solver.equalizer('btuaosu', 4)))
+    while True:
+        letters = input("Give me letters (ex. abba): ")
+        length = int(input("Give me word length: "))
+        print("Answers: ", "\n".join(solver.get_correct_words(letters, length)))
